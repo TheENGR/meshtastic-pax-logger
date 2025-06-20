7a868d78d7154d55c0a3cc7b2344838b60995069
TheENGR <TheENGR42@gmail.com>
2025-06-19 22:39:15 -0500

    Start of Plotter 2.0

--- plot_pax.py
+++ plot_pax.py
@@ -3,13 +3,26 @@ import matplotlib.dates as mdates
 import pandas as pd
 
 columns = ["Time","Bluetooth","Wifi","Total"]
-df = pd.read_csv('PAXLOG_FH_3_06.18.2025.csv', usecols=columns,)
+df = pd.read_csv('TEST.csv', usecols=columns,)
 
 #print("Contents in csv file:", df)
 
 df["Time"] = pd.to_datetime(df['Time'], format='%Y-%m-%dT%H:%M')
-df.plot(x="Time", y=["Bluetooth","Wifi","Total"],marker='o')
 
+#plot = df.plot(x="Time", y=["Bluetooth","Wifi","Total"],marker='o',grid=True)
+#plot.xaxis.set_major_locator(mdates.MinuteLocator(byminute=[0,15,30,45], interval = 1))
+#plot.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+
+plt.plot(df["Time"], df["Bluetooth"], marker='o', label="Bluetooth")
+plt.plot(df["Time"], df["Wifi"],      marker='o', label="Wifi")
+plt.plot(df["Time"], df["Total"],     marker='o', label="Total")
+
+plt.gca().xaxis.set(
+    major_locator=mdates.MinuteLocator(byminute=[0,15,30,45], interval = 1),
+    major_formatter=mdates.DateFormatter('%H:%M'),         # format minor ticks
+);
+
+plt.gcf().autofmt_xdate()
 plt.grid()
 plt.legend()
 plt.show()
